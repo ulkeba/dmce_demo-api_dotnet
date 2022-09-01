f9fcb5557f7ce01c24b3ee32f30a1c033343d240
Bastian Ulke <bastian.ulke@microsoft.com>
2022-09-01 16:04:21 +0200

    Retrieve JWT claims.

--- dotnet/Controllers/ConfidentialInfoController.cs
+++ dotnet/Controllers/ConfidentialInfoController.cs
@@ -10,12 +10,25 @@ namespace WeatherForecastAPI.Controllers;
 public class ConfidentialInfoController : ControllerBase
 {
 
+    private IHttpContextAccessor httpContextAccessor;
+
+    private String LogInUser = "<undefined>";
+
+    public ConfidentialInfoController(IHttpContextAccessor _httpContextAccessor) 
+    {
+        this.httpContextAccessor = _httpContextAccessor;
+        this.LogInUser = this.httpContextAccessor.HttpContext.User.Identity.Name;
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
+        var scopes = this.httpContextAccessor.HttpContext.User.Claims.Where(c => c.Type.Equals("http://schemas.microsoft.com/identity/claims/scope")).Select(c => c.Value).ToList();
         return Ok(new
         {
             confidentialInfo = "Don't tell anyone!",
+            logInUser = LogInUser,
+            scopes = scopes
         });
     }
 }
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddAuthentication().AddJwtBearer(options =>
     options.Authority = oidcAuthority;
 });
 
+builder.Services.AddHttpContextAccessor();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
